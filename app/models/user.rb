7511486267b6100adfca0d31825a1b1d8c648147
melnyk-r <kvart.38@ukr.net>
2017-09-18 19:33:56 +0300

    HW-49-1&2: text & username length, username format3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,10 @@ class User < ActiveRecord::Base
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
-  
+  validates :email, format: { with: /^[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+$/i }
+
   validates :username, length: { maximum: 40 }
-  validates :username, format: { with: /^[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+$/i }
+  validates :username, format: { with: /\A[a-zA-Z0-9\_]+\Z/ }
 
   attr_accessor :password
 
